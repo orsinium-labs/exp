722bf3e457cb82019028018857b822f2324d98b4
gram <mail@orsinium.dev>
2022-05-18 13:13:50 +0200

    inline functions

--- lib/s.ex
+++ lib/s.ex
@@ -2,13 +2,37 @@ defmodule S do
   @moduledoc """
   Documentation for `S`.
   """
-  use S.Eager
 
-  @decorate eager()
+  defmacro inline(call) do
+    {term, _} = Code.eval_quoted(call)
+    Macro.escape(term)
+  end
+
+  defmacrop maybe_inline(params, do: block) do
+    quote generated: true do
+      if Macro.quoted_literal?(unquote(params)) do
+        {term, _} = unquote(block) |> Code.eval_quoted()
+        Macro.escape(term)
+      else
+        unquote(block)
+      end
+    end
+  end
+
   @spec compile_regex(Macro.t()) :: Macro.t()
   defmacro compile_regex(source) do
-    quote bind_quoted: [source: source] do
-      Regex.compile!(source)
+    maybe_inline [source] do
+      quote generated: true do
+        Regex.compile!(unquote(source))
+      end
+    end
+  end
+
+  @spec unescape_string(Macro.t()) :: Macro.t()
+  defmacro unescape_string(str) do
+    quote bind_quoted: [str: str] do
+      require S
+      S.maybe_inline(Macro.unescape_string(str))
     end
   end
 end
--- test/s_test.exs
+++ test/s_test.exs
@@ -2,6 +2,33 @@ defmodule STest do
   use ExUnit.Case
   doctest S
 
+  describe "inline" do
+    test "runtime" do
+      assert S.inline(String.upcase("hello")) == "HELLO"
+    end
+
+    test "compile time" do
+      q = quote do: S.inline(String.upcase("hello"))
+      assert Macro.expand(q, __ENV__) == "HELLO"
+    end
+  end
+
+  # describe "maybe_inline" do
+  #   test "runtime" do
+  #     assert S.maybe_inline(String.upcase("hello")) == "HELLO"
+  #   end
+
+  #   test "compile time" do
+  #     q = quote do: S.maybe_inline(String.upcase("hello"))
+  #     assert Macro.expand(q, __ENV__) == "HELLO"
+  #   end
+
+  #   test "variable runtime" do
+  #     var = "hello"
+  #     assert S.maybe_inline(String.upcase(var)) == "HELLO"
+  #   end
+  # end
+
   describe "compile_regex" do
     test "runtime" do
       assert S.compile_regex("hello") == ~r"hello"
@@ -13,6 +40,7 @@ defmodule STest do
     end
 
     test "compile time" do
+      require S
       q = quote do: S.compile_regex("hello")
       assert Macro.expand(q, __ENV__) == Macro.escape(~r"hello")
     end
